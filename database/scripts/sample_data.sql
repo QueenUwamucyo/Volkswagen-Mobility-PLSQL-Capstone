9954fb461e610109f43d0de3c5b52a54304ecb57
Queen <queenwamucyo@gmail.com>
2025-12-07 20:00:12 +0200

    sample_data.sql
    sample datas

---
+++ database/scripts/sample_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO Customers VALUES (1, 'Eric Maniraguha', 'eric.maniraguha@auca.ac.rw', '+250788111222', 'Premium');
+INSERT INTO Customers VALUES (2, 'Uwamucyo Queen', 'queenwamucyo@gmail.com', '+2507923851', 'Gold');
+
+INSERT INTO Vehicles VALUES (1, 'VW ID.4', 'RAD123B', 'Available');
+INSERT INTO Vehicles VALUES (2, 'VW Polo', 'RAE456C', 'Available');
+
+INSERT INTO Drivers VALUES (1, 'John Doe', 'LIC12345');
+INSERT INTO Drivers VALUES (2, 'Alice Smith', 'LIC67890');
